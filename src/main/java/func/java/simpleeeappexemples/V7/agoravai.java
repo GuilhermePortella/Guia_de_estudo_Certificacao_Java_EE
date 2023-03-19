432754a45ea8848a8fa9100dd775eb1824706d37
GuilhermePortella <guilhermeportella2@gmail.com>
2023-03-18 22:53:03 -0300

    Sera

--- src/main/java/func/java/simpleeeappexemples/V7/agoravai.java
+++ src/main/java/func/java/simpleeeappexemples/V7/agoravai.java
@@ -9,6 +9,12 @@ public class agoravai {
         for (int i = 0; i < 10*3; i++) {
             System.out.println("numero " + i);
         }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Mundo, ola");
+            for (int j = 0; j < 10; j++) {
+                System.out.println("Mundo");
+            }
+        }
     }
     
 }
