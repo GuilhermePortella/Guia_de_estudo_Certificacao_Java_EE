8da98cccc7b6e834fbf02097311e640d5e89018a
GuilhermePortella <guilhermeportella2@gmail.com>
2023-03-21 23:34:25 -0300

    Finalizando v7

---
+++ src/main/java/func/java/simpleeeappexemples/V7/chat/JAXRSConfiguration.java
@@ -0,0 +1,14 @@
+package func.java.simpleeeappexemples.V7.chat;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Guilherme
+ */
+
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application{
+    
+}
